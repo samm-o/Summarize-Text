fb918c91287adc7edafb464046322fb57d18ccc7
samm-o <samuilsgeorgiev@gmail.com>
2023-01-30 09:52:25 -0500

    Add project files.

---
+++ Summarize Text/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Summarize_Text
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sentence = "This experiment is about a frog. This frog is red.";
+            var summary = String_Utility.SummarizeText(sentence, 30);
+            Console.WriteLine(summary);
+        }
+        
+    }
+}
